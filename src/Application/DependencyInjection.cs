c33985b02e4d68fbaaf492637a8974d817e57347
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-06-28 12:13:55 +0300

    Add DI

---
+++ src/Application/DependencyInjection.cs
@@ -0,0 +1,15 @@
+﻿namespace Application
+{
+    using AutoMapper;
+    using Microsoft.Extensions.DependencyInjection;
+    using System.Reflection;
+
+    public class DependencyInjection
+    {
+        public static IServiceCollection AddApplication(IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            return services;
+        }
+    }
+}
---
+++ src/Persistence/DependencyInjection.cs
@@ -0,0 +1,18 @@
+﻿namespace Persistence
+{
+    using Application.Common.Interfaces;
+    using Microsoft.EntityFrameworkCore;
+    using Microsoft.Extensions.DependencyInjection;
+    using Persistence.Context;
+
+    public class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(IServiceCollection services, IConnectionString connection)
+        {
+            services.AddDbContext<PawContext>(options => options.UseSqlServer(connection.Path));
+            services.AddScoped(provider => provider.GetService<IPawContext>());
+
+            return services;
+        }
+    }
+}
