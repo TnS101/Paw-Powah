903f38965821000e5b5ba5f06f1e0863bf352a78
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-06-28 10:18:49 +0300

    Add project files.

---
+++ Web UI/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Paw_Powah.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
