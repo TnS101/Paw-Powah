778bd2b6393901c4874aaf955e9e3583b5251a35
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-09 20:23:44 +0300

    Add Query Sorter

---
+++ src/Application/Common/Query Helpers/Sorter.cs
@@ -0,0 +1,19 @@
+﻿namespace Application.Common.Query_Helpers
+{
+    using System.Linq;
+
+    public class Sorter<T> where T : class
+    {
+        public IQueryable<T> Execute(IQueryable<T> collection, string criteria, string condition, double value) 
+        {
+            var property = collection.FirstOrDefault().GetType().GetProperty(criteria);
+            return condition switch
+            {
+                ">" => collection.Where(e => (double)e.GetType().GetProperty(criteria).GetValue(e, null) > value),
+                "<" => collection.Where(e => (double)e.GetType().GetProperty(criteria).GetValue(e, null) < value),
+                "=" => collection.Where(e => (double)e.GetType().GetProperty(criteria).GetValue(e, null) == value),
+                _ => collection,
+            };
+        }
+    }
+}
