31eb21bc73a1e702172a139f19a86a2acafb263e
TnS101 <54761133+TnS101@users.noreply.github.com>
2020-08-10 15:49:01 +0300

    Optimize Queries

--- src/Application/Services/Game/Items/ItemQueries.cs
+++ src/Application/Services/Game/Items/ItemQueries.cs
@@ -16,43 +16,18 @@
     public class ItemQueries : MapService<EquipableFullViewModel, ItemMinViewModel>, IItemQueries
     {
         public ItemQueries(IMapper mapper, IPawContext context)
-            :base(mapper, context)
+            : base(mapper, context)
         {
         }
 
         public async Task<IEnumerable<ItemMinViewModel>> GetInventory(long playerId)
         {
-            var amulets = await this.Context.PlayersAmulets.Where(p => p.PlayerId == playerId).Select(a => a.Amulet).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync();
-            var armors = await this.Context.PlayersArmors.Where(p => p.PlayerId == playerId).Select(a => a.Armor).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync();
-            var weapons = await this.Context.PlayersWeapons.Where(p => p.PlayerId == playerId).Select(w => w.Weapon).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync();
-            var consumeables = await this.Context.PlayersConsumeables.Where(p => p.PlayerId == playerId).Select(c => c.Consumeable).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync();
-
             var result = new HashSet<ItemMinViewModel>();
 
-            while (consumeables.Length > 0 && armors.Length > 0 && amulets.Length > 0 && weapons.Length > 0)
-            {
-
-                if (amulets.Length > 0)
-                {
-                    result.Add(amulets[^1]);
-                }
-
-                if (armors.Length > 0)
-                {
-                    result.Add(armors[^1]);
-                }
-
-                if (weapons.Length > 0)
-                {
-                    result.Add(weapons[^1]);
-
-                }
-
-                if (consumeables.Length > 0)
-                {
-                    result.Add(consumeables[^1]);
-                }
-            }
+            await this.Context.PlayersAmulets.Where(p => p.PlayerId == playerId).Select(a => a.Amulet).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ForEachAsync(e => result.Add(e));
+            await this.Context.PlayersArmors.Where(p => p.PlayerId == playerId).Select(a => a.Armor).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ForEachAsync(e => result.Add(e));
+            await this.Context.PlayersWeapons.Where(p => p.PlayerId == playerId).Select(w => w.Weapon).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ForEachAsync(e => result.Add(e));
+            await this.Context.PlayersConsumeables.Where(p => p.PlayerId == playerId).Select(c => c.Consumeable).ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ForEachAsync(e => result.Add(e));
 
             return result;
         }
@@ -69,7 +44,7 @@
                 "Amulet" => this.MapInfo(await this.Context.Amulets.FindAsync(id)),
                 "Armor" => this.MapInfo(await this.Context.Armors.FindAsync(id)),
                 "Weapon" => this.MapInfo(await this.Context.Weapons.FindAsync(id)),
-                _ => null,
+                _ => this.MapInfo(await this.Context.Consumeables.FindAsync(id)),
             };
         }
 
@@ -93,10 +68,10 @@
         {
             return type switch
             {
-                "Amulet" => await this.Context.Amulets.ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync(),
-                "Armor" => await this.Context.Armors.ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync(),
-                "Weapon" => await this.Context.Weapons.ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync(),
-                _ => await this.Context.Consumeables.ProjectTo<ItemMinViewModel>(this.Mapper.ConfigurationProvider).ToArrayAsync(),
+                "Amulet" => await this.MapCollection(this.Context.Amulets),
+                "Armor" => await this.MapCollection(this.Context.Armors),
+                "Weapon" => await this.MapCollection(this.Context.Weapons),
+                _ => await this.MapCollection(this.Context.Consumeables),
             };
         }
 
